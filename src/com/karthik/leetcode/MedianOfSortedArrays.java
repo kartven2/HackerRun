3b1956cd02baaa304600034ed6c9d9f8e12fa526
Karthik Venkataraman <kartven2@cisco.com>
2016-07-19 18:58:55 -0700

    LeetCode: https://leetcode.com/problems/median-of-two-sorted-arrays/

--- src/com/karthik/leetcode/MedianOfSortedArrays.java
+++ src/com/karthik/leetcode/MedianOfSortedArrays.java
@@ -19,83 +19,77 @@ public class MedianOfSortedArrays {
         MedianOfSortedArrays msa = new MedianOfSortedArrays();
         int[] nums1 = {};
         int[] nums2 = {0, 23};
-        System.out.println(msa.compute(nums1, nums2));
+        System.out.println(msa.findMedian(nums1, nums2));
     }
 
-    double mo2(double a, double b) {
+    private double mo2(double a, double b) {
         return (double) (a + b) / 2;
     }
 
-    double mo3(double a, double b, double c) {
-        return (a + b + c) - Math.max(a, Math.max(b, c))
-                - Math.min(a, Math.min(b, c));
+    private double mo3(double a, double b, double c) {
+        return (double) (a + b + c - Math.max(a, Math.max(b, c)) - Math.min(a, Math.min(b, c)));
     }
 
-    double mo4(double a, double b, double c, double d) {
-        double min = Math.min(a, Math.min(b, Math.min(c, d)));
-        double max = Math.max(a, Math.max(b, Math.max(c, d)));
-        return (double) (a + b + c + d - min - max) / 2;
+    private double mo4(double a, double b, double c, double d) {
+        return (double) (a + b + c + d - Math.max(a, Math.max(b, Math.max(c, d))) - Math.min(a, Math.min(b, Math.min(c, d)))) / 2;
     }
 
-    double median(int[] arr, int n) {
-        if (n == 0) {
-            return (double) n;
+    private double median(int[] arr, int N) {
+        if (N == 0) {
+            return (double) N;
         }
-        int a = n / 2;
-        if (n % 2 == 0) {
+        if (N == 1) {
+            return (double) arr[0];
+        }
+        int a = N / 2;
+        if (N % 2 == 1) {
+            return arr[a];
+        } else {
             return (double) (arr[a] + arr[a - 1]) / 2;
         }
-        return (double) arr[a];
     }
 
-    private double computeUtil(int[] A, int N, int[] B, int M) {
+    private double findMedian(int[] nums1, int[] nums2) {
+        return findMedianUtil(nums1, nums1.length, nums2, nums2.length);
+    }
+
+    private double findMedianUtil(int[] A, int N, int[] B, int M) {
         if (N <= M) {
             return findMedian(A, N, B, M);
         }
         return findMedian(B, M, A, N);
     }
 
-    public double compute(int[] nums1, int[] nums2) {
-        return computeUtil(nums1, nums1.length, nums2, nums2.length);
-    }
-
     private double findMedian(int[] A, int N, int[] B, int M) {
-        if (N == 0 && M == 0) {
-            return (double) N;
-        }
-
         if (N == 0) {
+            if (M == 0) {
+                return (double) N;
+            }
             return median(B, M);
         }
-
         if (N == 1) {
             if (M == 1) {
                 return mo2(A[0], B[0]);
             }
-            if (M % 2 == 1) {
-                return mo2(B[M / 2], mo3(B[(M / 2) - 1], B[(M / 2) + 1], A[0]));
+            if (M % 2 == 0) {
+                return mo3(A[0], B[(M / 2) - 1], B[M / 2]);
             }
-            return mo3(A[0], B[M / 2], B[(M / 2) - 1]);
+            return mo2(B[M / 2], mo3(A[0], B[(M / 2) - 1], B[(M / 2) + 1]));
         }
-
         if (N == 2) {
             if (M == 2) {
                 return mo4(A[0], A[1], B[0], B[1]);
             }
-
-            if (M % 2 == 1) {
-                return mo3(Math.max(A[0], B[(M / 2) - 1]), B[M / 2], Math.min(A[1], B[(M / 2) + 1]));
+            if (M % 2 == 0) {
+                return mo4(B[M / 2], Math.min(A[1], B[(M / 2) + 1]), Math.max(A[0], B[(M / 2) - 2]), B[(M / 2) - 1]);
             }
-
-            return mo4(B[M / 2], B[(M / 2) - 1], Math.max(A[0], B[(M / 2) - 2]), Math.min(A[1], B[(M / 2) + 1]));
+            return mo3(B[M / 2], Math.max(A[0], B[(M / 2) - 1]), Math.min(A[1], B[(M / 2) + 1]));
         }
-
-        int midAIdx = (N - 1) / 2;
-        int midBIdx = (M - 1) / 2;
-
-        if (A[midAIdx] <= B[midBIdx]) {
-            return computeUtil(Arrays.copyOfRange(A, midAIdx, N), (N / 2) + 1, B, M - midAIdx);
+        int midAidx = (N - 1) / 2;
+        int midBidx = (M - 1) / 2;
+        if (A[midAidx] <= B[midBidx]) {
+            return findMedianUtil(Arrays.copyOfRange(A, midAidx, N), (N / 2) + 1, B, M - midAidx);
         }
-        return computeUtil(A, (N / 2) + 1, Arrays.copyOfRange(B, midAIdx, M), M - midAIdx);
+        return findMedianUtil(A, (N / 2) + 1, Arrays.copyOfRange(B, midAidx, M), M - midAidx);
     }
 }
