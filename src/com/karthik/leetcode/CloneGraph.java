b1faa148e2212b854decae973ffd64634176adfd
Karthik Venkataraman <kartven2@cisco.com>
2016-11-08 17:15:54 -0800

    LeetCode Problem: Clone Graph
     * LeetCode: https://leetcode.com/problems/clone-graph/
     *
     * Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
     * OJ's undirected graph serialization:
     * Nodes are labeled uniquely.
     * We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
     * As an example, consider the serialized graph {0,1,2#1,2#2,2}.
     * The graph has a total of three nodes, and therefore contains three parts as separated by #.
     * First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
     * Second node is labeled as 1. Connect node 1 to node 2.
     * Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.

--- src/com/karthik/leetcode/CloneGraph.java
+++ src/com/karthik/leetcode/CloneGraph.java
@@ -9,7 +9,8 @@
  * The graph has a total of three nodes, and therefore contains three parts as separated by #.
  * First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
  * Second node is labeled as 1. Connect node 1 to node 2.
- * Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle. *
+ * Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+ *
  */
 package com.karthik.leetcode;
 
