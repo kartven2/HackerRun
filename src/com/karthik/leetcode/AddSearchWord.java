194980dc1c0757b620ffe0be9a91d1fe1d55e4d2
Karthik Venkataraman <kartven2@cisco.com>
2016-11-27 21:44:35 -0800

    LeetCode Problem: Add Search Word
    /*
     * LeetCode: https://leetcode.com/problems/add-and-search-word-data-structure-design/
     *
     * Design a data structure that supports the following two operations:
     * void addWord(word)
     * bool search(word)
     * search(word) can search a literal word or a regular expression string
     * containing only letters a-z or .. A . means it can represent any one letter.
     */

--- src/com/karthik/leetcode/AddSearchWord.java
+++ src/com/karthik/leetcode/AddSearchWord.java
@@ -39,19 +39,19 @@ public class AddSearchWord {
                 hasEmpty = true;
                 return;
             }
-            root = insert(root, word, 0);
+            root = insert(root, word, word.length(), 0);
         }
 
-        private Node insert(Node x, String word, int d) {
+        private Node insert(Node x, String word, int n, int d) {
             if (x == null) {
                 x = new Node();
             }
-            if (d == word.length()) {
+            if (d == n) {
                 x.end = true;
                 return x;
             }
             int idx = word.charAt(d) - 'a';
-            x.ch[idx] = insert(x.ch[idx], word, d + 1);
+            x.ch[idx] = insert(x.ch[idx], word, n, d + 1);
             return x;
         }
 
@@ -62,26 +62,26 @@ public class AddSearchWord {
             if (word.trim().isEmpty()) {
                 return hasEmpty;
             }
-            return search(root, word, 0);
+            return search(root, word, word.length(), 0);
         }
 
-        private boolean search(Node x, String word, int d) {
+        private boolean search(Node x, String word, int n, int d) {
             if (x == null) {
                 return false;
             }
-            if (word.length() == d) {
+            if (n == d) {
                 return x.end;
             }
             char c = word.charAt(d);
             if (c == '.') {
                 for (int i = 0; i < 26; i++) {
-                    if (x.ch[i] != null && search(x.ch[i], word, d + 1)) {
+                    if (x.ch[i] != null && search(x.ch[i], word, n, d + 1)) {
                         return true;
                     }
                 }
                 return false;
             }
-            return search(x.ch[c - 'a'], word, d + 1);
+            return search(x.ch[c - 'a'], word, n, d + 1);
         }
     }
 
