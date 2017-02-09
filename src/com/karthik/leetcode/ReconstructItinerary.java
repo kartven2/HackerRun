e3773c05b6f5760ea5339c50dfb222e036e55c7d
Karthik Venkataraman <kartven2@cisco.com>
2017-02-08 16:24:28 -0800

    In Progress

--- src/com/karthik/leetcode/ReconstructItinerary.java
+++ src/com/karthik/leetcode/ReconstructItinerary.java
@@ -14,10 +14,156 @@
  */
 package com.karthik.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * @author Karthik Venkataraman
  * @email kafy83@gmail.com
  */
 public class ReconstructItinerary {
-    
+
+    private static final int R = 26;
+    private static final char FC = 'A';
+    private static final String JFK = "JFK";
+    private static final Map<String, Integer> MAP = new HashMap<>();
+    private static String[] array;
+
+    static class Lsd {
+
+        private void sort(String[] a) {
+            if (a == null || a.length == 0) {
+                return;
+            }
+            int n = a.length;
+            String[] aux = new String[n];
+            for (int d = 2; d >= 0; d--) {
+                int[] count = new int[R + 1];
+                for (int i = 0; i < n; i++) {
+                    count[a[i].charAt(d) - FC + 1]++;
+                }
+                for (int r = 0; r < R; r++) {
+                    count[r + 1] += count[r];
+                }
+                for (int i = 0; i < n; i++) {
+                    aux[count[a[i].charAt(d) - FC]++] = a[i];
+                }
+                for (int i = 0; i < n; i++) {
+                    a[i] = aux[i];
+                }
+            }
+        }
+    }
+
+    static class Domain {
+
+        private int[] a;
+        private int i;
+
+        Domain(List<Integer> b) {
+            int sz = b == null ? 0 : b.size();
+            this.a = new int[sz];
+            if (b == null) {
+                return;
+            }
+            int k = 0;
+            for (int x : b) {
+                this.a[k++] = x;
+            }
+            Arrays.sort(a);
+        }
+
+        private boolean hasNext() {
+            return i < a.length;
+        }
+
+        private int next() {
+            return a[i++];
+        }
+    }
+
+    private Set<String> getDistinctCitySet(int n, String[][] tickets) {
+        final Set<String> src = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                src.add(tickets[i][j]);
+            }
+        }
+        return src;
+    }
+
+    private void assignCityIdx(String[] citys) {
+        int sz = 0;
+        array = new String[citys.length + 1];
+        MAP.put(JFK, sz);
+        array[sz++] = JFK;
+        for (String city : citys) {
+            MAP.put(city, sz);
+            array[sz++] = city;
+        }
+    }
+
+    private Domain[] getDomains(int n, int nc, String[][] tickets) {
+        List<Integer>[] adj = (List<Integer>[]) new LinkedList[nc];
+        for (int i = 0; i < n; i++) {
+            String from = tickets[i][0];
+            String to = tickets[i][1];
+            int fromIdx = MAP.get(from);
+            int toIdx = MAP.get(to);
+            if (adj[fromIdx] == null) {
+                adj[fromIdx] = new LinkedList<>();
+            }
+            adj[fromIdx].add(toIdx);
+        }
+        Domain[] domain = new Domain[nc];
+        for (int i = 0; i < nc; i++) {
+            domain[i] = new Domain(adj[i]);
+        }
+        return domain;
+    }
+
+    public List<String> findItinerary(String[][] tickets) {
+        List<String> result = new LinkedList<>();
+        if (tickets == null || tickets.length == 0) {
+            return result;
+        }
+        int n = tickets.length;
+        Set<String> citySet = getDistinctCitySet(n, tickets);
+        if (!citySet.contains(JFK)) {
+            return result;
+        }
+        int nc = citySet.size();
+        String[] a = new String[nc - 1];
+        int sz = 0;
+        for (String city : citySet) {
+            if (!city.equals(JFK)) {
+                a[sz++] = city;
+            }
+        }
+        Lsd lsd = new Lsd();
+        lsd.sort(a);
+        assignCityIdx(a);
+        Domain[] domain = getDomains(n, nc, tickets);
+        Stack<String> path = new Stack<>();
+        Stack<Integer> stk = new Stack<>();
+        stk.push(0);
+        while (!stk.isEmpty()) {
+            int v = stk.pop();
+            while (domain[v].hasNext()) {
+                stk.push(v);
+                v = domain[v].next();
+            }
+            path.push(array[v]);
+        }
+        while (!path.isEmpty()) {
+            result.add(path.pop());
+        }
+        return result;
+    }
 }
