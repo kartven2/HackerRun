c2b40303a2cc279b7c87dd7a5d9154536382aa36
Karthik <kafy83@gmail.com>
2017-03-19 23:51:52 -0700

    In progress

---
+++ src/com/karthik/leetcode/ContinuousSubarraySum.java
@@ -0,0 +1,49 @@
+/*
+ * LeetCode: https://leetcode.com/problems/continuous-subarray-sum/#/description
+ * Given a list of non-negative numbers and a target integer k,
+ * write a function to check if the array has a continuous subarray of size
+ * at least 2 that sums up to the multiple of k,
+ * that is, sums up to n*k where n is also an integer.
+ * The length of the array won't exceed 10,000.
+ * You may assume the sum of all the numbers is
+ * in the range of a signed 32-bit integer.
+ */
+package com.karthik.leetcode;
+
+/**
+ * @author Karthik Venkataraman
+ * @email kafy83@gmail.com
+ */
+public class ContinuousSubarraySum {
+
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return k == 0;
+        }
+        if (k < 0) {
+            k = k * (-1);
+        }
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                int sum = 0;
+                for (int l = j; l < n && l < j + i + 1; l++) {
+                    sum += nums[l];
+                }
+                if (k == 0 && sum == 0) {
+                    return true;
+                }
+                if (k != 0 && sum % k == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String... args) {
+        ContinuousSubarraySum cc = new ContinuousSubarraySum();
+        int input[] = {23, 2, 4, 6, 7};
+        System.out.println(cc.checkSubarraySum(input, 6));
+    }
+}
