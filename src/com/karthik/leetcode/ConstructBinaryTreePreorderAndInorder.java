17bebce43ade2eb1e8a8028f3a3d9f4f3175cab8
Karthik <kafy83@gmail.com>
2017-03-28 00:38:06 -0700

    In Progress

--- src/com/karthik/leetcode/ConstructBinaryTreePreorderAndInorder.java
+++ src/com/karthik/leetcode/ConstructBinaryTreePreorderAndInorder.java
@@ -30,38 +30,78 @@ public class ConstructBinaryTreePreorderAndInorder {
         }
     }
 
-    private TreeNode buildTree(int[] preorder, int[] inorder, int pi, TreeNode x) {
-        if(pi>=preorder.length) {
-            
-        }
-        if(x==null) {
-            x = new TreeNode(preorder[pi]);
-        }
-        List<Integer> left = new ArrayList<>();
-        int j=0;
-        for(int i=0; i<inorder.length && inorder[i] != preorder[pi]; i++) {
-            left.add(inorder[i]);
-            j=i;
-        }
-        j+=2;
-        int[] larr = new int[left.size()];
-        for(int i=0; i<larr.length; i++) {
-            larr[i] = left.get(i);
-        }        
-        x.left = buildTree(preorder, larr, pi+1, null);
-        int[] rarr = new int[inorder.length-j];
-        for(int k=0;j<inorder.length; j++, k++) {
-            rarr[k] = inorder[j];
-        }
-        x.right = buildTree(preorder)
+    public static void main(String...args) {
+        ConstructBinaryTreePreorderAndInorder cb = new ConstructBinaryTreePreorderAndInorder();
+        cb.buildTree(new int[] {2,1}, new int[] {1,2});
     }
     
-    
+    class Multimap {
+
+        Map<Integer, Integer> m1 = new HashMap<>();
+        Map<Integer, Integer> m2 = new HashMap<>();
+
+        void add(int key, int idx) {
+            m1.put(key, idx);
+            m2.put(idx, key);
+        }
+
+        int getIdx(int key) {
+            return m1.get(key);
+        }
+
+        int getKey(int idx) {
+            return m2.get(idx);
+        }
+    }
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || inorder == null || preorder.length != inorder.length) {
+        if (preorder == null || inorder == null || preorder.length != inorder.length || preorder.length == 0) {
             return null;
         }
-        return buildTree(preorder, inorder, 0, null);
+
+        Multimap pre = new Multimap();
+        for (int i = 0; i < preorder.length; i++) {
+            pre.add(preorder[i], i);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int x : inorder) {
+            list.add(x);
+        }
+        return buildTree(pre, 0, null, list);
+    }
+
+    private TreeNode buildTree(Multimap pre, Integer pi, TreeNode x, List<Integer> list) {
+        int root = pre.getKey(pi);
+        if (x == null) {
+            x = new TreeNode(root);
+        }
+        if (list.size() == 1) {
+            return x;
+        }
+        List<Integer> leftChld = new ArrayList<>();
+        List<Integer> rightChld = new ArrayList<>();
+        List<Integer> ptr = leftChld;
+        Integer lidx = null, ridx = null, idx = null;
+        for (int key : list) {
+            if (key == root) {
+                ptr = rightChld;
+                lidx = idx;
+                idx = null;
+            } else {
+                ptr.add(key);
+                if (idx==null || pre.getIdx(key) < idx) {
+                    idx = pre.getIdx(key);
+                }
+            }
+        }
+        ridx = idx;
+        if (lidx!=null) {
+            x.left = buildTree(pre, lidx, null, leftChld);
+        }
+        if (ridx!=null) {
+            x.right = buildTree(pre, ridx, null, rightChld);
+        }
+        return x;
     }
 }
-
