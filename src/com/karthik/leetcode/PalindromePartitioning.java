b283b742ba30262b5141df9e8b3d3dc81398e13c
Karthik <kafy83@gmail.com>
2017-04-02 14:34:08 -0700

    LeetCode Problem: Palindrome Partitioning
    /*
     * https://leetcode.com/problems/palindrome-partitioning/#/description
     *
     * Given a string s, partition s such that every substring of the partition is a palindrome.
     * Return all possible palindrome partitioning of s.
     * For example, given s = "aab",
     */

--- src/com/karthik/leetcode/PalindromePartitioning.java
+++ src/com/karthik/leetcode/PalindromePartitioning.java
@@ -7,6 +7,7 @@
  */
 package com.karthik.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,15 +23,49 @@ public class PalindromePartitioning {
         }
         int n = s.length();
         boolean[][] dp = new boolean[n][n];
-        for (int i = 0, k = 0; i < n; i++, k++) {
-            for (int j = i; j + k < n; j++) {
-                if (k == 0) {
-                    dp[j][j + k] = true;
-                } else if (s.charAt(j) == s.charAt(j + k) && dp[j + 1][j + k - 1]) {
-                    dp[j][j + k] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j + i < n; j++) {
+                if (i == 0) {
+                    dp[j][j] = true;
+                    if (j + 1 < n) {
+                        dp[j + 1][j] = true;
+                    }
+                } else if (s.charAt(j) == s.charAt(j + i) && dp[j + 1][j + i - 1]) {
+                    dp[j][j + i] = true;
                 }
             }
         }
+
+        List<Integer>[] list = (List<Integer>[]) new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (dp[i][j]) {
+                    if (list[i] == null) {
+                        list[i] = new ArrayList<>();
+                    }
+                    list[i].add(j);
+                }
+            }
+        }
+
+        collect(result, new ArrayList<>(), s, 0, list);
+
         return result;
+    }
+
+    private void collect(List<List<String>> result, List<String> split, String s, int i, List<Integer>[] list) {
+        if (i >= s.length()) {
+            List<String> nl = new ArrayList<>();
+            for (String x : split) {
+                nl.add(x);
+            }
+            result.add(nl);
+            return;
+        }
+        for (int end : list[i]) {
+            split.add(s.substring(i, ++end));
+            collect(result, split, s, end, list);
+            split.remove(split.size() - 1);
+        }
     }
 }
